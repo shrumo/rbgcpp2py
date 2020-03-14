80834c94124e55c1f973572cba0b3d463b3692fd
Maksymilian Mika <shrum@oldie>
2020-03-14 16:06:34 +0000

    Changing compilation flag and the script

--- scripts/create_game_library.sh
+++ scripts/create_game_library.sh
@@ -16,7 +16,7 @@ make --directory=${0%/*}/../rbg2cpp
 ${0%/*}/../rbg2cpp/bin/rbg2cpp -o rbg_game $1
 mv ./rbg_game.cpp ${0%/*}/../rbg_game.cpp
 mv ./rbg_game.hpp ${0%/*}/../rbg_game.hpp
-cmake ${0%/*}/.. -B ${0%/*}/../build
+cmake -S ${0%/*}/.. -B ${0%/*}/../build
 make --directory=${0%/*}/../build
 rm ${0%/*}/../rbg_game.cpp
 rm ${0%/*}/../rbg_game.hpp
