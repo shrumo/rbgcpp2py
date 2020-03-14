ad426675596fce6f1e50a83f977f089275bac62a
Maksymilian Mika <shrum@oldie>
2020-03-14 14:39:01 +0000

    Added benchmark capabilities to utils.

--- scripts/create_game_library.sh
+++ scripts/create_game_library.sh
@@ -6,14 +6,17 @@
 
 echo "Creating rbg_game python library for $1"
 
-git submodule init --recursive
-git submodule update --recursive
+git -C ${0%/*}/../ submodule init
+git -C ${0%/*}/../ submodule update
+
+git -C ${0%/*}/../rbg2cpp submodule init 
+git -C ${0%/*}/../rbg2cpp submodule update
 
 make --directory=${0%/*}/../rbg2cpp
 ${0%/*}/../rbg2cpp/bin/rbg2cpp -o rbg_game $1
 mv ./rbg_game.cpp ${0%/*}/../rbg_game.cpp
 mv ./rbg_game.hpp ${0%/*}/../rbg_game.hpp
-cmake .. -B ${0%/*}/../build
+cmake ${0%/*}/.. -B ${0%/*}/../build
 make --directory=${0%/*}/../build
 rm ${0%/*}/../rbg_game.cpp
 rm ${0%/*}/../rbg_game.hpp
--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 import rbg_game
+import random
+import time
 
 bitarraystack = rbg_game.resettable_bitarray_stack()
 
@@ -26,3 +28,43 @@ def perft(state, depth):
     result[0] += rec_result[0]
     result[1] += rec_result[1]
   return result
+
+# Play the game till the end from the current state. 
+# This modifies the given state.
+def playout(state):
+  keeper_closure(state)
+  moves = state.get_all_moves(bitarraystack)
+  nodes = 1
+  while moves:
+    move = random.choice(moves)
+    state.apply_move(move)
+    keeper_closure(state)
+    moves = state.get_all_moves(bitarraystack)
+    nodes += 1
+  return [state.get_player_score(i) for i in range(1,rbg_game.number_of_players())], nodes
+
+__RBG_BENCHMARK_PLAYOUTS__=1000
+__RBG_BENCHMARK_DEPTH__=3
+def benchmark():
+  points = [0 for i in range(1,rbg_game.number_of_players())]
+  nodes = 0
+  begin = time.time()
+  for i in range(__RBG_BENCHMARK_PLAYOUTS__):
+    results, nodes_visited = playout(rbg_game.game_state())
+    for player, score in enumerate(results):
+      points[player] += score
+      nodes += nodes_visited
+    if i % 100 == 0:
+      print('Played out',i,'out of',__RBG_BENCHMARK_PLAYOUTS__)
+  end = time.time()
+  print('Done',__RBG_BENCHMARK_PLAYOUTS__,'games in',end-begin,'s')
+  print('Visited',nodes,'nodes (',nodes/(end - begin),' nodes/sec)')
+  print('Average scores:')
+  for player, score in enumerate(points):
+    print(player+1,':',score/__RBG_BENCHMARK_PLAYOUTS__)
+  begin = time.time()
+  result = perft(rbg_game.game_state(), __RBG_BENCHMARK_DEPTH__)
+  end = time.time()
+  print('Calculated perft to depth',__RBG_BENCHMARK_DEPTH__,'in',end-begin,'s')
+  print('Visited',results[1],'nodes (',nodes/(end - begin),' nodes/sec)')
+  print('There are',results[0],'leaves and',results[1],'nodes')
